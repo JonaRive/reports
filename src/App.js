afdee727ee570e3fc0c197f95b8c361de64540a0
VictorH <pandvictor@gmail.com>
2023-06-15 11:39:38 -0600

    Mod: se forma de parametros

--- src/App.js
+++ src/App.js
@@ -2,39 +2,45 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 
-import Linear from './components/charts/Linear';
-import Pie from './components/charts/Pie';
+import Linear from "./components/charts/Linear";
+import Pie from "./components/charts/PieCustom";
 
-import './App.css';
+import "./App.css";
+const labelsPie = [
+  "Palencia",
+  "Villa Nueva",
+  "Jalapa",
+  "Chimaltenango",
+  "San Marcos",
+  "Mixco",
+  "Mita",
+  "Zacapa",
+  "Quiche",
+  "Xela",
+];
 
-
-
-
+const dataPie = [50, 140, 600, 750, 1500, 1200, 900, 250, 750, 600];
 function App() {
-
-  
   return (
     <>
-    
-    
-     <NavBar /> 
-     <div className="flex"> 
-      <SideBar />
-        <div className="content"> 
-        <Routes>
-            <Route path="/linear" exact={true} Component={Linear} />
-            <Route path="/pie" exact={true} Component={Pie} />    
+      <NavBar />
+      <div className='flex'>
+        <SideBar />
+        <div className='content'>
+          <Routes>
+            <Route path='/linear' exact={true} Component={Linear} />
+            <Route
+              path='/pie'
+              exact={true}
+              Component={Pie}
+              title={"Departamento"}
+              labels={labelsPie}
+              data={dataPie}
+            />
           </Routes>
         </div>
       </div>
-     
-
-      </>
-    
-    
-    
-      
-    
+    </>
   );
 }
 
