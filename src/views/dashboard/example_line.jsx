1c0950953207e895c152ef72b74b60da2334a9f7
VictorH <pandvictor@gmail.com>
2023-06-15 11:49:29 -0600

    add: ejemplo linea view

---
+++ src/views/dashboard/example_line.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Linear from "../../components/charts/Linear";
+
+const Example_Line =()=>{
+
+
+    return <>
+        <Linear/>
+        <Linear/>
+        <Linear/>
+    </>
+}
